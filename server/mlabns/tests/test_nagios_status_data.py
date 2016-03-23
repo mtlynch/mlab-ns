e575770ffa58f1d349acb1fe8795d38d94d20bd8
Michael Lynch <mtlynch@google.com>
2016-03-23 12:07:06 -0400

    poc commit

--- server/mlabns/tests/test_nagios_status_data.py
+++ server/mlabns/tests/test_nagios_status_data.py
@@ -33,29 +33,15 @@ class GetNagiosCredentialsTest(unittest2.TestCase):
         self.testbed.init_datastore_v3_stub()
         self.testbed.init_memcache_stub()
 
-        nagios_model_patch = mock.patch.object(model, 'Nagios', autospec=True)
-        self.addCleanup(nagios_model_patch.stop)
-        nagios_model_patch.start()
-
     def tearDown(self):
         self.testbed.deactivate()
 
     def test_get_nagios_credentials_returns_successfully_from_memcache(self):
-        #mock_nagios = mock.Mock()
-        #memcache.set(constants.DEFAULT_NAGIOS_ENTRY, mock_nagios)
+        nagios_model = model.Nagios(url='foo')
+        memcache.set(constants.DEFAULT_NAGIOS_ENTRY, nagios_model)
 
-        #actual_nagios = nagios_status_data.get_nagios_credentials()
-
-        #self.assertNotEqual(actual_nagios, None)
-        #self.assertEqual(constants.DEFAULT_NAGIOS_ENTRY, actual_nagios.key_id)
-
-        with mock.patch('google.appengine.api.memcache') as mock_memcache:
-            mock_memcache.get.return_value = mock.Mock(
-                key_id=constants.DEFAULT_NAGIOS_ENTRY)
-            #actual_nagios= nagios_status_data.get_nagios_credentials()
-            #self.assertNotEqual(actual_nagios, None)
-            #self.assertEqual(constants.DEFAULT_NAGIOS_ENTRY, actual_nagios.key_id)
-
+        retrieved = nagios_status_data.get_nagios_credentials()
+        self.assertEqual(retrieved.url, 'foo')
 
 if __name__ == '__main__':
     unittest2.main()
